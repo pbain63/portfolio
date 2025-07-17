d3923940af7d9988a19d84d4fa86c8696dead6d5
pbain63 <prodipbain63@gmail.com>
2025-07-17 18:44:21 +0600

    Add JS code to by clicking 'back to top' button page got to top and show smooth behavior in main.js

--- main.js
+++ main.js
@@ -26,3 +26,24 @@ window.addEventListener("resize", () => {
     isOpen = false;
   }
 });
+
+//// Back to Top button
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
